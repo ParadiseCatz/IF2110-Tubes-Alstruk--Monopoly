f4238b4eb88ed1e391779feb5278dae288538676
Ngiong <ngiooong@yahoo.com>
2015-11-14 14:16:45 +0700

    include petak.h dan player.h, add global_stackOfDefeated

--- globalvariable.h
+++ globalvariable.h
@@ -1,5 +1,9 @@
-ListOfPlayer global_listOfPLayer;
+#include "ADT/header/player.h"
+#include "ADT/header/petak.h"
+
+ListOfPlayer global_listOfPlayer;
 ListOfPetak global_listOfPetak;
 Player *global_currentPlayer;
 Petak *global_currentWorldCup;
 QueueOfKartu global_queueOfKartu;
+StackOfPlayer global_stackOfDefeated;
