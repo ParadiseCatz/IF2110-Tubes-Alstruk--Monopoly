67cfe01231b76a724d1cddf6a351662de3acc9a1
Thea Olivia <theaolivia.mail@gmail.com>
2015-11-24 06:21:20 +0700

    edited main
    edited main

--- ADT/driver/cards_driver.c
+++ ADT/driver/cards_driver.c
@@ -3,7 +3,9 @@
 int main(){
 
 	InfoKartu Ctemp;
-	Queue Qtemp;
+	Deck Qtemp;
+	*Hand *Htemp;
+	int choice;
 	
 	printf("Masukkan pilihan : \n");
 	printf("=================================");
@@ -11,6 +13,28 @@ int main(){
 	printf("2. Activate Card\n");
 	printf("3. Sell Card\n")
 	printf("4. Buy Card\n");
+
+	switch(choice){
+		case '1' :
+			break;
+		case '2' :
+			Dealokasi(Qtemp);
+
+			GetID(Qtemp);
+
+			switch(GetID(*Htemp)){
+
+
+			}
+
+			break;
+		case '3' :
+			break;
+		case '4' :
+			break:
+		default :
+			printf("Silahkan memasukkan pilihan 1, 2, 3, atau 4\n");
+	}
 	
 	return 0;
 }
