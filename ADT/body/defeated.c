bee7f032f1df010a790929cb07f13da4c7f3550f
anwarramadha <anwar.ramadha@gmail.com>
2015-11-24 12:51:33 +0700

    stack untuk menampung pemain yang sudah kalah

---
+++ ADT/body/defeated.c
@@ -0,0 +1,20 @@
+#include "../header/defeated.h"
+
+void CreateEmptyS(StackOfPlayer *S) {
+    Top(*S)=Nil;
+}
+
+void Push (StackOfPlayer *S, InfoPlayer X) {
+    AddressOfPlayer P=Alokasi(X);
+	if(P!=Nil){
+		Next(P)=Top(*S);
+		Top(*S)=P;
+	}
+}
+
+void Pop (StackOfPlayer *S, InfoPlayer *X){
+    *X=InfoTop(*S);
+	AddressOfPlayer P=Top(*S);
+	Top(*S)=Next(P);
+	Dealokasi(&P);
+}
