20598eea08276cdeb0512d0dbd7aaf463662fff6
ParadiseCatz <ChristianAnthonySetyawan@gmail.com>
2015-11-29 18:37:36 +0700

    Fix method name

--- ADT/header/gamesystem.h
+++ ADT/header/gamesystem.h
@@ -1,7 +1,7 @@
 #ifndef GAME_H
 #define GAME_H
 #include "../../globalvariable.h"
-void game_system_start();
+void gamesystem_start();
 void gamesystem_show_help();
 void gamesystem_do_action(UserAction userAction, Kata parameter);
 int gamesystem_roll_dice(int diceCount);
